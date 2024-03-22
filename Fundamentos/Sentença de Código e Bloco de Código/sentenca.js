8363136fe441fd9cf4bc121cc212bf6bdf801252
samuel.almeida <anjogfx@gmail.com>
2024-03-21 22:02:28 -0300

    Material Inicial

---
+++ Fundamentos/Sentença de Código e Bloco de Código/sentenca.js
@@ -0,0 +1,18 @@
+// Cada sentença de código é como uma frase;
+console.log("Bom dia");
+console.log("Boa tarde");
+console.log("Boa noite");
+// No exemplo acima há três sentenças de código;
+// O ";" não é necessário, porém, indica que para o programador o fim da sentença;
+
+// O console.log() é um método do objeto global window (ou do objeto global) chamado console;
+console.log(1 * Math.random());  
+// Aqui estamos usando a função random() do Math dentro do console.log(), retornando uma multiplicação;;
+
+// A ordem do código importa!
+console.log("Passo 01");
+console.log("Passo 02");
+console.log("Passo 03");
+
+// Para realizar anotações  em um programa você pode usar os comentários;
+// Continuar guia de introdução a programação no próximo arquivo.
---
+++ Fundamentos/comentario.js
@@ -0,0 +1,13 @@
+// Um comentário pode ser usado para explicar o que um bloco de código faz, ou para dar sugestões ao programador.
+
+/* 
+    O comentário pode ser feito também desta forma;
+    A diferença é que aqui posso quebrar linhas;
+*/ 
+
+console.log("Passo 01");
+console.log("Passo 02");
+console.log("Passo 03");
+// console.log("Passo 04"); Não executado, pois é um comentário;
+
+
